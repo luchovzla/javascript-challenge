cafbfdafc9dba780af7c15dc02c445fcd393fef3
Luis Gomez <luis.g.1990@gmail.com>
2021-04-11 18:21:38 -0500

    Populated table with data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,4 +3,17 @@ console.log("app.js loaded successfully!");
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Reference to table body
+var table = d3.select("#ufo-table");
+var tbody = table.append('tbody');
+
+// Loop through 'data' and print out UFO sightings, appending a row for each element
+data.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append('tr');
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
